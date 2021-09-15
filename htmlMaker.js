fd7e4519863ef24e37511ea82c6ee8f4389f8aff
Gustavo Tavares <71890326+GMOTGIT@users.noreply.github.com>
2021-09-15 17:01:27 -0300

    Change on the createReadStream to readFile

--- htmlMaker.js
+++ htmlMaker.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const { argv } = require("process");
 
 module.exports = htmlMaker = (txtInput, argv_o, argv_s, argv_i) => {
   //Ignore non txt files
@@ -12,43 +13,48 @@ module.exports = htmlMaker = (txtInput, argv_o, argv_s, argv_i) => {
 
   if (argv_i.indexOf(":") != -1) {
     var isFolder = fs.statSync(argv_i);
-    var s = fs.createReadStream(
-      `${isFolder.isDirectory() ? argv_i + "/" + txtInput : txtInput}`
-    );
+    var s = `${isFolder.isDirectory() ? argv_i + "/" + txtInput : txtInput}`;
   } else {
     var isFolder = fs.statSync("./" + argv_i);
-
-    var s = fs.createReadStream(
-      `${isFolder.isDirectory() ? "./" + argv_i + "/" + txtInput : txtInput}`
-    );
   }
 
   //https://github.com/yargs/yargs/blob/HEAD/docs/examples.md
   //Yargs is here to help you...
 
-  let lines = [];
+  //   var s = fs.createReadStream(
+  //     `${isFolder.isDirectory() ? "./" + argv_i + "/" + txtInput : txtInput}`
+  //   );
 
-  s.on("data", function (buf) {
-    lines = buf.toString().split(/\r?\n\r?\n/);
-  });
+  //s.on Bugging on The Naval Treaty
+  // s.on("data", function (buf) {
+  // lines = buf.toString().split(/\r?\n\r?\n/);
+  // });
 
-  s.on("end", function () {
-    let paragraphs = "";
-    let title = `<h1>${lines.shift()}</h1>`;
-    //let title = `<h1>${lines.shift()}</h1></br>`;
+  // s.on("end", function () {
 
-    lines.forEach((string) => {
-      paragraphs += `<p>${string}</p>`;
-      //paragraphs += `<p>${string}</p></br>`;
-    });
+  fs.readFile(
+    `${isFolder.isDirectory() ? argv_i + "/" + txtInput : txtInput}`,
+    "utf8",
+    (err, lines) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      let paragraphs = "";
 
-    let styleSheet = argv_s
-      ? `<link rel="stylesheet" href=${argv_s}></link>`
-      : "";
+      lines = lines.toString().split(/\r?\n\r?\n/);
 
-    let fileName = txtInput.split(".txt").shift();
+      let fileName = lines.shift();
+      lines.forEach((string) => {
+        paragraphs += `<p>${string}</p>\n`;
+      });
 
-    const content = `<!doctype html>
+      let h1 = fileName ? `<h1>${fileName}</h1>` : "";
+      let styleSheet = argv_s
+        ? `<link rel="stylesheet" href=${argv_s}></link>`
+        : "";
+
+      const content = `<!doctype html>
 <html lang="en">
 <head>
   <meta charset="utf-8">
@@ -57,25 +63,31 @@ module.exports = htmlMaker = (txtInput, argv_o, argv_s, argv_i) => {
 ${styleSheet}
 </head>
 <body>
-${title}
+${h1}
 ${paragraphs}
 </body>
 </html>`;
 
-    //Check for output
+      if (argv_o) {
+        if (argv_o.indexOf("./") == -1) {
+          if (txtInput.includes("\\")) {
+            //check if receiving an absolute path
+            let index = txtInput.lastIndexOf("\\");
+            let path = txtInput.substring(txtInput.length, index);
+            txtInput = path.split(".txt").shift();
+            console.log(txtInput);
+          }
 
-    //Check if output is inside the main folder
-    //if outside then save it there
-    if (argv_o) {
-      if (argv_o.indexOf("./") == -1) {
-        if (txtInput.includes("\\")) {
-          //check if receiving an absolute path
-          let index = txtInput.lastIndexOf("\\");
-          let path = txtInput.substring(txtInput.length, index);
-          txtInput = path;
+          fs.writeFile(`${argv_o}/${fileName}.html`, content, (err) => {
+            if (err) {
+              console.error(err);
+              return;
+            }
+          });
+          return;
         }
 
-        fs.writeFile(`${argv_o}/${fileName}.html`, content, (err) => {
+        fs.writeFile(`./${argv_o}/${fileName}.html`, content, (err) => {
           if (err) {
             console.error(err);
             return;
@@ -84,44 +96,29 @@ ${paragraphs}
         return;
       }
 
-      fs.writeFile(`./${argv_o}/${fileName}.html`, content, (err) => {
-        if (err) {
-          console.error(err);
-          return;
-        }
-      });
-      return;
-    }
+      //Check if input file has absolute path
+      if (txtInput.includes("\\")) {
+        let index = txtInput.lastIndexOf("\\");
+        var path = txtInput.substring(txtInput.length, index);
 
-    // //check here
-    //Check if input file has absolute path
-    if (txtInput.includes("\\")) {
-      let index = txtInput.lastIndexOf("\\");
-      let path = txtInput.substring(txtInput.length, index);
-
-      fs.writeFile(
-        `${"./dist/" + path.split(".txt").shift()}.html`,
-        content,
-        (err) => {
+        fs.writeFile(`${"./dist/" + fileName}.html`, content, (err) => {
           if (err) {
             console.error(err);
 
-            console.log("IHERE");
             return;
           }
-        }
-      );
-    }
+        });
+        return;
+      }
 
-    //check if input file is in root folder
-    if (!txtInput.includes("\\")) {
-      //default output - dist folder created before
       fs.writeFile(`${"./dist/" + fileName}.html`, content, (err) => {
         if (err) {
           console.error(err);
+
           return;
         }
+        return;
       });
     }
-  });
+  );
 };
