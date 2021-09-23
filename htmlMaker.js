8548e8fdf19b45fd6ad3790d3f99a036cd9865ec
Leyang Yu <september437@gmail.com>
2021-09-22 20:59:50 -0400

    Updating regex and HTML for headers based on review feedback

--- htmlMaker.js
+++ htmlMaker.js
@@ -49,11 +49,11 @@ module.exports = htmlMaker = (txtInput, argv_o, argv_s, argv_i) => {
           string = string
             .replace(/\*{2,}(.*?)\*{2,}/g, '<b>$1</b>')
             .replace(/\*(.*?)\*/g, '<i>$1</i>')
-            .replace(/^# (.*$)/, '<h1>$1</h1>')
-            .replace(/^## (.*$)/, '<h2>$1</h2>')
+            .replace(/^##((.|\s)*$)/, '<h2>$1</h2>')
+            .replace(/^#((.|\s)*$)/, '<h1>$1</h1>')
             .replace(/\[(.*?)\]\((.*?)\)/g, '<a href="$2">$1</a>');
         }
-        paragraphs += `<p>${string}</p>\n`;
+        paragraphs += (string.match(/<h1>/) || string.match(/<h2>/)) ? `${string}\n` : `<p>${string}</p>\n`;
       });
 
       let h1 = title ? `<h1>${title}</h1>` : "";
